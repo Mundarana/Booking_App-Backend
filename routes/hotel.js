b00d72bd713c29f7e1919456e2cb3a2c477bb0a3
Mohammad Awais Rana <mundarana11@gmail.com>
2023-07-04 15:42:01 +0200

    fixed bug in hotel route

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require("express");
 
-const {createHotel, updateHotel, deleteOneHotel, getOneHotel, getAllHotels, uploadPhotos} = require('../controllers/hotel');
+const {
+  createHotel,
+  updateHotel,
+  deleteOneHotel,
+  getOneHotel,
+  getAllHotels,
+  uploadPhotos,
+} = require("../controllers/hotel");
 
 const api = express.Router();
 
-api.route("/").post(createHotel).get(getAllHotels).post(uploadPhotos);
-api.route('/:id').put(updateHotel).delete(deleteOneHotel).get(getOneHotel);
+api.route("/").post(createHotel).get(getAllHotels);
+api.route("/:id").put(updateHotel).delete(deleteOneHotel).get(getOneHotel);
 
-module.exports = api
+module.exports = api;
