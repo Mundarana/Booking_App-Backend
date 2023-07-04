1ef1038a02a1be7e1e11215d5f186ca46dceb8b7
Mohammad Awais Rana <mundarana11@gmail.com>
2023-07-04 15:48:41 +0200

    fixed potential photos bug

--- models/Photo.js
+++ models/Photo.js
@@ -1,19 +1,16 @@
 const mongoose = require("mongoose");
 
-const PhotosSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String
-    },
-    photos: {
-      type :[String],
-    },
-    hotel: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Hotel',
-      required: true
-    },
-  }
-);
+const PhotosSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  photos: {
+    type: [String],
+  },
+  hotel: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Hotel",
+  },
+});
 
-module.exports = mongoose.model('Photo', PhotosSchema);
+module.exports = mongoose.model("Photo", PhotosSchema);
